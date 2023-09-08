7752394f98d9552c82a02b83e2b154dfa8277413
sarahmonem <sarahamonem@gmail.com>
2023-09-07 19:00:37 -0700

    size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'size of types'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char:%zu\n", sizeof(char));
+	return (0);
+}
