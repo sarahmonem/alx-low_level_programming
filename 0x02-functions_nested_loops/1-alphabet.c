46bb47c868ef9ba01a17d0c1ce0e3217194a4d2d
sarahmonem <sarahamonem@gmail.com>
2023-09-12 16:18:37 -0700

    alphabit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * main- entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Alway 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z', ch++)
+		print_alphabet(_putchar(ch));
+	_putchar('\n');
+
+	return(0);
