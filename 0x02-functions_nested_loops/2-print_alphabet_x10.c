451959ab8b81a56d4cf6bb3b9d71b588038c2553
sarahmonem <sarahamonem@gmail.com>
2023-09-12 16:42:13 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function for print alphabet using _putchar
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+	int i = 0;
+
+	for (i = 0; i <= 10; i++){
+		    for (ch = 'a'; ch <= 'z'; ch++)
+                _putchar(ch);
+        _putchar('\n');
+	}
+}
