11e1827df1934a96dec058e9ac7c2dbecf89bc96
sarahmonem <sarahamonem@gmail.com>
2023-09-15 15:26:44 -0700

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- *Description: code for check uppercase
- *
  * _isupper: check if ch is uppercase
  *
  *@c: input character
  *
  *Return: return1 if ch is upper and 0 if else
  */
+
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,6 @@
 
 int _putchar(char c);
 int _isupper(int c);
+int _isupper(int c)
 
 #endif
