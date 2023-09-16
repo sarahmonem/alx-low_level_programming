8432767d45419828fbedd0c9d9e0b5eeb089b363
sarahmonem <sarahamonem@gmail.com>
2023-09-15 17:33:57 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,20 @@
 */
 void more_numbers(void)
 {
-	char i;
-	int j;
+	int i, j, k;
 
-	for (j = 0; j <10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 48; i <= 57 ; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i);
+			k = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar(k + 48);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
