8563e9fc3bca0cc1bb6c0ff09eba1976e173fdf8
sarahmonem <sarahamonem@gmail.com>
2023-09-22 16:35:36 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strlen - return the length of string
+ *
+ * @s : pointer parameter
+ *
+ * Return: length of string
+ *
+ *
+ *
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s !='\0'; s++)
+		count++;
+
+	return(counter);
+}
