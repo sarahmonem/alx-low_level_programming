ef9d43fc45054a1bd91bac37373e36b862bfa63e
sarahmonem <sarahamonem@gmail.com>
2023-10-01 11:06:17 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,9 +11,9 @@
 
 int is_prime_number(int n)
 {
-	if (n % n == 0 && n / 1 == n)
-		return (1);
-	else if (n == 1 || n < 0)
+	if (n == 1 || n < 0)
+		return (0);
+	else if (n % n == 0 && n / 1 == n)
 		return (0);
 	else
 		return (0);
