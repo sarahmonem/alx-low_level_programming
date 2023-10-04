f0f6986bd01c5360be5d31d67ff1f486804e041a
sarahmonem <sarahamonem@gmail.com>
2023-10-04 01:48:19 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,20 +16,18 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 1;
 
-	if( argc == 3)
+	if (argc == 3)
 	{
-		 for (i = 1; i < argc; i++)
-                {
-
-                sum = atoi(argv[i]) * atoi(argv[i+1]);
-                printf("%d\n", sum);
-                }
+		for (i = 1; i < argc; i++)
+		{
+			sum = atoi(argv[i]) * atoi(argv[i + 1]);
+			printf("%d\n", sum);
+		}
 	}
 	else
 	{
 		printf("Error\n");
 	}
-	
 
 	return (1);
 }
